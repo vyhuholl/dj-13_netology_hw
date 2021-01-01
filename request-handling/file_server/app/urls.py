4b52994b09d52149e7dea423fedca7d5408041d0
vyhuholl <olga-p-98@mail.ru>
2021-01-02 01:15:51 +0300

    finally done file_server

--- request-handling/file_server/app/urls.py
+++ request-handling/file_server/app/urls.py
@@ -1,12 +1,12 @@
-from django.urls import path
-
-# Определите и зарегистрируйте конвертер для определения даты в урлах и наоборот урла по датам
+from django.urls import re_path
+from app.views import file_list, file_content
 
 
 urlpatterns = [
-    # Определите схему урлов с привязкой к отображениям .views.file_list и .views.file_content
-    # path(..., name='file_list'),
-    # path(..., name='file_list'),    # задайте необязательный параметр "date"
-                                      # для детальной информации смотрите HTML-шаблоны в директории templates
-    # path(..., name='file_content'),
+    re_path(
+        r'^(?P<date>[0-9]{4}-[0-9]{2}-[0-9]{2})?/?$', file_list,
+        name='file_list'),
+    re_path(
+        r'^file/(?P<name>.*\.[\w\d]{2,5})/?$', file_content,
+        name='file_content')
 ]
--- request-handling/file_server/app/views.py
+++ request-handling/file_server/app/views.py
@@ -1,29 +1,28 @@
-import datetime
-
+import os
+from datetime import datetime
+from django.conf import settings
 from django.shortcuts import render
 
 
-def file_list(request):
-    template_name = 'index.html'
-    
-    # Реализуйте алгоритм подготавливающий контекстные данные для шаблона по примеру:
-    context = {
-        'files': [
-            {'name': 'file_name_1.txt',
-             'ctime': datetime.datetime(2018, 1, 1),
-             'mtime': datetime.datetime(2018, 1, 2)}
-        ],
-        'date': datetime.date(2018, 1, 1)  # Этот параметр необязательный
-    }
-
-    return render(request, template_name, context)
+def file_list(request, date=None):
+    if date:
+        date = datetime.strptime(date, '%Y-%m-%d')
+    files = []
+    for name in sorted(os.listdir(settings.FILES_PATH)):
+        stat_result = os.stat(os.path.join(settings.FILES_PATH, name))
+        ctime = datetime.fromtimestamp(stat_result.st_ctime)
+        mtime = datetime.fromtimestamp(stat_result.st_mtime)
+        if not date or date.date() in [ctime.date(), mtime.date()]:
+            files.append({'name': name, 'ctime': ctime, 'mtime': mtime})
+    return render(request, 'index.html', context={
+        'files': files, 'date': date})
 
 
 def file_content(request, name):
-    # Реализуйте алгоритм подготавливающий контекстные данные для шаблона по примеру:
-    return render(
-        request,
-        'file_content.html',
-        context={'file_name': 'file_name_1.txt', 'file_content': 'File content!'}
-    )
-
+    if name not in os.listdir(settings.FILES_PATH):
+        content = 'File not found!'
+    else:
+        with open(os.path.join(settings.FILES_PATH, name)) as curr_file:
+            content = curr_file.read()
+    return render(request, 'file_content.html', context={
+        'file_name': name, 'file_content': content})
