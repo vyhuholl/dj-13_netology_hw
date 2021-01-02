aea7330f4e95bff4d0c6fbebcf6896f4d1a20751
vyhuholl <olga-p-98@mail.ru>
2021-01-02 19:34:42 +0300

    inflation_russia.csv now is parsed correctly

--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -4,11 +4,20 @@ from django.conf import settings
 from django.shortcuts import render
 
 
+def transform(key, value):
+    if not value:
+        return '-'
+    return float(value) if key != 'Год' else value
+
+
 def inflation_view(request):
     file_path = os.path.join(settings.BASE_DIR, 'inflation_russia.csv')
     with open(file_path) as csv_file:
-        reader = DictReader(csv_file, delimiter=';', restval='-')
+        reader = DictReader(csv_file, delimiter=';')
         fieldnames = reader.fieldnames
-        data = [row.values() for row in reader]
+        data = [
+            [transform(key, value) for key, value in row.items()]
+            for row in reader
+            ]
     return render(request, 'inflation.html', context={
         'fieldnames': fieldnames, 'data': data})
