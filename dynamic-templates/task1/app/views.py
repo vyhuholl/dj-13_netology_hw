04d63d5c9ed38c1f3e67fbd20b77daf138704a1a
vyhuholl <olga-p-98@mail.ru>
2021-01-02 20:12:36 +0300

    rewrote transform(), added get_type()

--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -1,13 +1,24 @@
 import os.path
+from datetime import date
 from csv import DictReader
+from django import template
 from django.conf import settings
 from django.shortcuts import render
+
+register = template.Library()
+
+
+@register.filter
+def get_type(obj):
+    return type(obj).__name__
 
 
 def transform(key, value):
     if not value:
-        return '-'
-    return float(value) if key != 'Год' else value
+        return None
+    elif key == 'Год':
+        return date(value, 1, 1)
+    return float(value) if key != 'Суммарная' else value
 
 
 def inflation_view(request):
