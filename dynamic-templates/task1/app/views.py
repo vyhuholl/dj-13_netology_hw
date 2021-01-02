e237310ac66c0ebdb32b754faaaa542f5a69bc29
vyhuholl <olga-p-98@mail.ru>
2021-01-02 18:45:25 +0300

    added correct delimiter=';' to DictReadler

--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render
 def inflation_view(request):
     file_path = os.path.join(settings.BASE_DIR, 'inflation_russia.csv')
     with open(file_path) as csv_file:
-        reader = DictReader(csv_file, restval='-')
+        reader = DictReader(csv_file, delimiter=';', restval='-')
         fieldnames = reader.fieldnames
         data = [row.values() for row in reader]
     return render(request, 'inflation.html', context={
