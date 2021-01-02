20514614c45f587fbe5b677092889d495c5a60bf
vyhuholl <olga-p-98@mail.ru>
2021-01-02 18:34:11 +0300

    finally finished html for task1

--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -1,10 +1,14 @@
+import os.path
+from csv import DictReader
+from django.conf import settings
 from django.shortcuts import render
 
+
 def inflation_view(request):
-    template_name = 'inflation.html'
-
-    # чтение csv-файла и заполнение контекста
-    context = {}
-
-    return render(request, template_name,
-                  context)
+    file_path = os.path.join(settings.BASE_DIR, 'inflation_russia.csv')
+    with open(file_path, encoding='cp1251') as csv_file:
+        reader = DictReader(csv_file, restval='-')
+        fieldnames = reader.fieldnames
+        data = [row.values() for row in reader]
+    return render(request, 'inflation.html', context={
+        'fieldnames': fieldnames, 'data': data})
