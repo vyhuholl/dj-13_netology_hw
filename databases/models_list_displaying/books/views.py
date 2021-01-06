0b41c59362aa6848ef599d8da41a79f3ad44c486
vyhuholl <olga-p-98@mail.ru>
2021-01-06 15:56:11 +0300

    kinda done something

--- databases/models_list_displaying/books/views.py
+++ databases/models_list_displaying/books/views.py
@@ -1,7 +1,36 @@
+from datetime import datetime
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from books.models import Book
 
 
-def books_view(request):
-    template = 'books/books_list.html'
-    context = {}
-    return render(request, template, context)
+# скрипт converters.py я удалила, потому что
+# все преобразования в и из datetime реализованы здесь
+
+
+class BookListView(ListView):
+    model = Book
+    template_name = 'books_list.html'
+    ordering = ['pub_date']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        pub_date = self.request.query_params.get('pub_date', None)
+        if pub_date:
+            queryset = Book.objects.all()
+            pub_date = datetime.strptime(pub_date, '%Y-%m-%d')
+            less = queryset.filter(pub_date__lte=pub_date)
+            greater = queryset.filter(pub_date__gte=pub_date)
+            if less:
+                context['prev'] = less[-1].pub_date.strftime('%Y-%m-%d')
+            if greater:
+                context['next'] = greater[0].pub_date.strftime('%Y-%m-%d')
+        return context
+
+    def get_queryset(self):
+        queryset = Book.objects.all()
+        pub_date = self.request.query_params.get('pub_date', None)
+        if pub_date:
+            pub_date = datetime.strptime(pub_date, '%Y-%m-%d')
+            queryset = queryset.filter(pub_date__date=pub_date)
+        return queryset
--- databases/models_list_displaying/main/urls.py
+++ databases/models_list_displaying/main/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 
-from books.views import books_view
+from books.views import BookListView
 
 urlpatterns = [
-    path('', books_view, name='books'),
+    re_path(
+        r'^books/(?P<pub_date>[0-9]{4}-[0-9]{2}-[0-9]{2})?/?$',
+        BookListView.as_view(), name='books'),
     path('admin/', admin.site.urls),
 ]
