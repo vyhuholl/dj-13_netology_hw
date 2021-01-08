0c6d66c249c7fda471d6825e38ecab73a4b38faf
vyhuholl <olga-p-98@mail.ru>
2021-01-08 12:33:56 +0300

    minor fix in views

--- databases_2/m2m-relations/articles/views.py
+++ databases_2/m2m-relations/articles/views.py
@@ -7,7 +7,6 @@ class ArticleToScopeListView(ListView):
 
     model = ArticleToScope
     template_name = 'articles/news.html'
-    ordering = '-published_at'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
