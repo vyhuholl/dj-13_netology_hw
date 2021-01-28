840953b696eeed76bb30db75f7ac12bfca79891e
vyhuholl <olga-p-98@mail.ru>
2021-01-28 18:56:11 +0300

    simple_crud 1st additional

--- drf-intro/simple_crud/measurements/models.py
+++ drf-intro/simple_crud/measurements/models.py
@@ -1,28 +1,35 @@
 from django.db import models
 
 
-class Project(models.Model):
+# Параметр abstract = True указывает, что этот класс не создаёт новую
+# таблицу в БД, а добавляет указанные в нём поля в таблицы для классов,
+# унаследованных от этого класса. Если не указать этот параметр, то
+# поля по-прежнему будут добавляться в унаследованные классы, но
+# при этом для самого класса будет создаваться новая таблица в БД.
+
+
+class TimestampFields(models.Model):
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    class Meta:
+        abstract = True
+
+
+class Project(TimestampFields):
     """Объект на котором проводят измерения."""
 
     name = models.TextField()
     latitude = models.FloatField()
     longitude = models.FloatField()
-    created_at = models.DateTimeField(
-        auto_now_add=True
-    )
-    updated_at = models.DateTimeField(
-        auto_now=True
-    )
 
 
-class Measurement(models.Model):
+class Measurement(TimestampFields):
     """Измерение температуры на объекте."""
 
     value = models.FloatField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(
-        auto_now_add=True
-    )
-    updated_at = models.DateTimeField(
-        auto_now=True
-    )
