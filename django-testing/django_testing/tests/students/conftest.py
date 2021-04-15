f280873c2b8805bb40d32003b7df61a24f62bd36
vyhuholl <olga-p-98@mail.ru>
2021-04-15 12:21:38 +0300

    added student_factory and course_factory fixtures

--- django-testing/django_testing/tests/students/conftest.py
+++ django-testing/django_testing/tests/students/conftest.py
@@ -1,2 +1,16 @@
 import pytest
 from model_bakery import baker
+
+
+@pytest.fixture
+def student_factory():
+    def factory(**kwargs):
+        return baker.make('Student', **kwargs)
+    return factory
+
+
+@pytest.fixture
+def course_factory():
+    def factory(**kwargs):
+        return baker.make('Course', **kwargs)
+    return factory
